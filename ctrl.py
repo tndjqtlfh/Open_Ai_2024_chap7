bae5e06887d5fb2c06946f4b6f14c64d64e7d685
unknown <fr0925@naver.com>
2024-04-18 11:02:34 +0900

    Remove pow,mod function and operator - light version

--- ctrl.py
+++ ctrl.py
@@ -17,10 +17,6 @@ class Control:
                 return f'{num1} * {num2} = {self.mul(num1, num2)}'
             elif operator == '/':
                 return f'{num1} / {num2} = {self.div(num1, num2)}'
-            elif operator == '^':
-                return f'{num1} ^ {num2} = {self.pow(num1, num2)}'
-            elif operator == '%':
-                return f'{num1} % {num2} = {self.mod(num1, num2)}'
             else :
                 return "Calculation Error"
         
@@ -49,24 +45,5 @@ class Control:
             return e
         
         return a/b
-    
-    def pow(self, a, b):
-        try:
-           if (a==0):
-                raise Exception("Base Error")
-            
-        except Exception as e:
-            return e
-        
-        return pow(a, b)   
-    
-    def mod(self, a, b):
-        try:
-            if(b==0):
-                raise Exception("Divisor Error")
-            
-        except Exception as e:
-            return e
-        
-        return a%b
+
     
